f4964e79163ca4e1fefa8cd0d8bdaba786b05e5b
Brian Cloutier <github@briancloutier.com>
2018-10-15 19:10:46 -0700

    Add RoutingTable implementation gleaned from the wrong paper

---
+++ core.py
@@ -0,0 +1,155 @@
+from __future__ import annotations  # allow forward references (PEP 563)
+
+import collections
+import datetime
+import ipaddress
+import random
+import typing
+
+def new_node_id() -> int:
+    'a random nodeid from [0, 2^160-1]'
+    nodeid = random.getrandbits(160)
+    assert nodeid.bit_length() <= 160
+    return nodeid
+
+def nodeid_as_bitstring(nodeid: int) -> str:
+    return f"{nodeid:0160b}"
+
+def node_distance(left: int, right: int) -> int:
+    return left ^ right
+
+class Node(typing.NamedTuple):
+    addr: typing.Union[ipaddress.IPv4Address, ipaddress.IPv6Address]
+    port: int
+    nodeid: int
+
+class RoutingEntry(typing.NamedTuple):
+    node: Node
+    last_seen: datetime.datetime
+
+    def update_last_seen(self):
+        now = datetime.datetime.utcnow()
+        return self._replace(last_seen=now)
+
+class NoRoomInBucket(Exception):
+    '''
+    Raised by RoutingTable.node_seen, indicates we should try to evict entry
+    '''
+    def __init__(self, entry: RoutingEntry):
+        self.entry = entry
+
+class RoutingTable:
+    Bucket = typing.MutableMapping[int, RoutingEntry]  # Actually, an OrderedDict
+
+    def __init__(self, k: int, mynodeid: int):
+        self.k = k
+        self.nodeid = mynodeid
+
+        Buckets = typing.List[self.Bucket]
+        self.buckets: Buckets = collections.defaultdict(collections.OrderedDict)
+
+    def _bucket_index_for(self, nodeid: int) -> int:
+        assert self.nodeid != nodeid
+        assert nodeid >= 0
+        assert nodeid <= (2**160 - 1)
+
+        distance = node_distance(self.nodeid, nodeid)
+        index = distance.bit_length() - 1
+
+        assert index >= 0 and index < 160, index
+        return index
+
+    def _bucket_for(self, nodeid: int) -> RoutingTable.Bucket:
+        bucket_index = self._bucket_index_for(nodeid)
+        return self.buckets[bucket_index]
+    
+    def last_seen_for(self, nodeid: int) -> datetime.datetime:
+        bucket_index = self._bucket_index_for(nodeid)
+        bucket = self.buckets[bucket_index]
+        entry = bucket[nodeid]  # may raise KeyError if nodeid is not known
+        return entry.last_seen
+
+    @staticmethod
+    def _i_centered_indexes(i: int, length: int) -> typing.Iterator[int]:
+        'Generates a permutation of range(length)'
+        assert(length > 0)
+        yield (i,)
+        width = 1
+        while True:
+            to_return = tuple()
+            if (i-width) >= 0:
+                to_return += (i-width,)
+            if (i+width) < length:
+                to_return += (i+width,)
+
+            if len(to_return):
+                yield to_return
+            else:
+                break
+            width += 1
+
+    def closest(self, targetnodeid: int) -> typing.List[Node]:
+        'Returns the k nodes we know of which are closest to key'
+        bucket_index = self._bucket_index_for(targetnodeid)
+
+        nodes = []
+        for indexes in self._i_centered_indexes(bucket_index, len(self.buckets)):
+            # indexes is a tuple of buckets to check next
+            buckets = [self.buckets[index] for index in indexes]
+            new_nodes = (entry.node for bucket in buckets for entry in bucket.values())
+            closest = sorted(
+                new_nodes,
+                key=lambda node: node_distance(targetnodeid, node.nodeid)
+            )
+            nodes.extend(closest)
+
+            if len(nodes) >= self.k:
+                return nodes[:self.k]
+
+        # we weren't able to find k nodes so we'll return every node we have
+        return nodes
+
+    def closest_to_me(self) -> typing.List[Node]:
+        'Returns up to k nodes which are closest to self.nodeid'
+        nodes = []
+        for bucket in self.buckets:
+            new_nodes = (entry.node for entry in bucket.values())
+            closest = sorted(
+                new_nodes,
+                key=lambda node: node_distance(self.nodeid, node.nodeid)
+            )
+            nodes.extend(closest)
+
+            if len(nodes) >= self.k:
+                return nodes[:self.k]
+
+    @staticmethod
+    def _first_element_of_ordered_dict(dictionary: collections.OrderedDict):
+        # There has to be a better way to peek() an OrderedDict
+        assert(len(dictionary) > 0)
+        return next(iter(dictionary.items()))
+
+    def node_seen(self, node: Node):
+        assert self.nodeid != node.nodeid
+
+        bucket: collections.OrderedDict = self._bucket_for(node.nodeid)
+
+        if node.nodeid in bucket:
+            bucket[node.nodeid] = bucket[node.nodeid].update_last_seen()
+            bucket.move_to_end(node.nodeid)
+            return
+
+        if len(bucket) < self.k:
+            now = datetime.datetime.utcnow()
+            entry = RoutingEntry(node=node, last_seen=now)
+            bucket[node.nodeid] = entry
+            return
+
+        nodeid, routing_entry = self._first_element_of_ordered_dict(bucket)
+        raise NoRoomInBucket(routing_entry)
+
+    def evict_node(self, nodeid: int):
+        'Removes this node from the routing table'
+        bucket: collections.OrderedDict = self._bucket_for(nodeid)
+        del bucket[nodeid]
+
---
+++ test_core.py
@@ -0,0 +1,142 @@
+import collections
+import pytest
+import typing
+
+from core import *
+
+
+def test_bucket_index_for():
+    nodeid = 0b1000
+    table = RoutingTable(2, nodeid)
+
+    assert table._bucket_index_for(0b1001) == 0  # distance of 1 -> first bucket
+    assert table._bucket_index_for(0b1010) == 1  # distance of 2 -> second bucket
+    assert table._bucket_index_for(0b1011) == 1  # distance of 3 -> second bucket
+    assert table._bucket_index_for(0b1100) == 2  # distance of 3 -> third bucket
+
+    nodeid = 0
+    table = RoutingTable(2, nodeid)
+
+    assert table._bucket_index_for(2**160 - 1) == 159  # this belongs in the last bucket
+
+
+def test_first_element_of_ordered_dict():
+    dictionary = collections.OrderedDict()
+    dictionary[10] = 1
+    dictionary[20] = 2
+
+    assert RoutingTable._first_element_of_ordered_dict(dictionary) == (10, 1)
+    dictionary.move_to_end(10)
+    assert RoutingTable._first_element_of_ordered_dict(dictionary) == (20, 2)
+
+
+def test_i_centered_indexes():
+    assert list(RoutingTable._i_centered_indexes(0, 1)) == [(0,)]
+    assert list(RoutingTable._i_centered_indexes(3, 5)) == [(3,), (2, 4), (1,), (0,)]
+
+    def flatten(tups: typing.Iterator[typing.Tuple[int, ...]]):
+        return [subitem for item in tups for subitem in item]
+
+    # it should return a permutation of range(length)
+    assert sorted(flatten(RoutingTable._i_centered_indexes(73, 160))) == list(range(160))
+
+
+def test_calling_node_seen_bumps_last_seen():
+    mynodeid = 0b1000
+    table = RoutingTable(2, mynodeid)
+
+    one_id = 0b1010
+    one = Node('localhost', 9000, one_id)
+
+    table.node_seen(one)
+    old_last_seen = table.last_seen_for(one_id)
+
+    table.node_seen(one)
+    new_last_seen = table.last_seen_for(one_id)
+
+    assert old_last_seen < new_last_seen
+
+
+def test_filling_bucket_triggers_exception():
+    mynodeid = 0b1000
+    table = RoutingTable(2, mynodeid)
+
+    one_id = 0b1100
+    one = Node('localhost', 1, one_id)
+
+    two_id = 0b1101
+    two = Node('localhost', 2, two_id)
+
+    three_id = 0b1110
+    three = Node('localhost', 3, three_id)
+
+    table.node_seen(one)
+    table.node_seen(two)
+
+    with pytest.raises(NoRoomInBucket):
+        table.node_seen(three)
+
+
+def test_can_evict_from_bucket():
+    mynodeid = 0b1000
+    table = RoutingTable(2, mynodeid)
+
+    one_id = 0b1100
+    one = Node('localhost', 1, one_id)
+
+    two_id = 0b1101
+    two = Node('localhost', 2, two_id)
+
+    three_id = 0b1110
+    three = Node('localhost', 3, three_id)
+
+    table.node_seen(one)
+    table.node_seen(two)
+
+    table.evict_node(one_id)
+    table.node_seen(three)  # does not raise NoRoomInBucket
+
+def test_closest():
+    mynodeid = 0b10000
+    table = RoutingTable(2, mynodeid)
+
+    def mknode(nodeid):
+        return Node('localhost', 1, nodeid)
+
+    table.node_seen(mknode(0b10010))
+    assert table._bucket_index_for(0b10010) == 1
+
+    # if there are fewer than k entries, we return all of them!
+    assert len(table.closest(0b111110100)) == 1
+
+    table.node_seen(mknode(0b10100))
+    assert table._bucket_index_for(0b10100) == 2
+
+    table.node_seen(mknode(0b10101))
+    assert table._bucket_index_for(0b10101) == 2
+
+    table.node_seen(mknode(0b11000))
+    assert table._bucket_index_for(0b11000) == 3
+
+    table.node_seen(mknode(0b110000))
+    assert table._bucket_index_for(0b110000) == 5
+
+    # bucket 1 isn't full, so we have to take a node from bucket 2 as well
+    nodes = table.closest(0b10010)
+    assert [node.nodeid for node in nodes] == [0b10010, 0b10100]
+
+    # bucket 2 _is_ full, so we return all the elements from it
+    nodes = table.closest(0b10100)
+    assert [node.nodeid for node in nodes] == [0b10100, 0b10101]
+
+    table.evict_node(0b10100)
+
+    # bucket 2 is no longer full, so we look at the buckets nearby
+    nodes = table.closest(0b10100)
+    assert [node.nodeid for node in nodes] == [0b10101, 0b10010]
+
+    # there are two buckets nearby and we return the node which is closest (by xor)
+    nodes = table.closest(0b10101)
+    assert node_distance(mynodeid, 0b10010) < node_distance(mynodeid, 0b11000)
+    assert [node.nodeid for node in nodes] == [0b10101, 0b10010]
+
