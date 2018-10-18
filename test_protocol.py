eb27f78e8dd4873286f4ba249a1da515ad632a03
Brian Cloutier <github@briancloutier.com>
2018-10-17 22:12:56 -0700

    Pull out mock server creation into a helper

--- test_protocol.py
+++ test_protocol.py
@@ -25,80 +25,6 @@ def test_read_write_nodeid():
     assert nodeid == involve(nodeid)
 
 
-@pytest.mark.asyncio  # run the test inside an event loop so we don't have to make one
-async def test_nonce_matching():
-    'When you send a PING and get back a PONG with the same nonce the future is triggered'
-
-    local_node = core.Node(addr='localhost', port=3000, nodeid=100)
-
-    server = protocol.Server(k=2, mynodeid=100)
-    await server.listen(3000)
-
-    ping_message = protocol.create_ping(local_node)
-    future = server.send(ping_message, ('localhost', 3001))
-
-    # this is a lot of code just to send a UDP packet
-    loop = asyncio.get_running_loop()
-    transport, dgprotocol = await loop.create_datagram_endpoint(
-        asyncio.DatagramProtocol,
-        local_addr = ('localhost', 3001)
-    )
-
-    await asyncio.sleep(0.1)
-    assert not future.done()  # we have not yet sent the message
-
-    remote_node = core.Node(addr='localhost', port=3001, nodeid=110)
-
-    pong_message = protocol.create_pong(remote_node, ping_message.nonce)
-    pong_message.nonce = b'garbage'
-    serialized = pong_message.SerializeToString()
-    transport.sendto(serialized, ('localhost', 3000))
-
-    await asyncio.sleep(0.1)
-    assert not future.done()  # we sent a message with the wrong nonce
-
-    pong_message = protocol.create_pong(remote_node, ping_message.nonce)
-    serialized = pong_message.SerializeToString()
-    transport.sendto(serialized, ('localhost', 3000))
-
-    # we sent a conforming PONG, the future should now be set!
-    try:
-        await asyncio.wait_for(future, timeout=0.5)
-    except asyncio.TimeoutError:
-        assert False, 'the future was never set'
-
-    assert future.done()
-
-
-@pytest.mark.asyncio
-async def test_incoming_messages_notify_routing_table():
-    'When we receive a message we tell the routing table about it'
-
-    # 1. Start an empty server
-    server = protocol.Server(k=2, mynodeid=0b1000)
-    await server.listen(3000)
-
-    remoteid = 0b1001
-
-    # 2. Send it a PONG
-    loop = asyncio.get_running_loop()
-    transport, dgprotocol = await loop.create_datagram_endpoint(
-        asyncio.DatagramProtocol,
-        local_addr = ('localhost', 3001)
-    )
-    remote_node = core.Node(addr='localhost', port=3001, nodeid=remoteid)
-    pong_message = protocol.create_pong(remote_node, b'garbage')
-    serialized = pong_message.SerializeToString()
-    transport.sendto(serialized, ('localhost', 3000))
-
-    # 3. Look for the node in the routing table and see that it is good
-    with pytest.raises(KeyError):
-        server.table.last_seen_for(remoteid)
-
-    await asyncio.sleep(0.1)
-    assert server.table.last_seen_for(remoteid) is not None
-
-
 class RecordingDatagramProtocol(asyncio.DatagramProtocol):
     def __init__(self):
         self.messages = list()
@@ -108,32 +34,112 @@ class RecordingDatagramProtocol(asyncio.DatagramProtocol):
         self.messages.append(message)
 
 
+class MockServer:
+    def __init__(self, transport, protocol):
+        self.transport = transport
+        self.protocol = protocol
+    def send(self, addr, message):
+        serialized = message.SerializeToString()
+        self.transport.sendto(serialized, addr)
+    @property
+    def messages(self):
+        return self.protocol.messages
+
+
+async def startmockserver():
+    'Binds a server to port 3001'
+    loop = asyncio.get_running_loop()
+    transport, proto = await loop.create_datagram_endpoint(
+        RecordingDatagramProtocol, local_addr = ('localhost', 3001)
+    )
+    return MockServer(transport, proto)
+
+
+@pytest.mark.asyncio  # run the test inside an event loop so we don't have to make one
+async def test_nonce_matching():
+    'When you send a PING and get back a PONG with the same nonce the future is triggered'
+    mockserver = await startmockserver()
+
+    local_node = core.Node(addr='localhost', port=3000, nodeid=100)
+
+    server = protocol.Server(k=2, mynodeid=100)
+    await server.listen(3000)
+
+    ping_message = protocol.create_ping(local_node)
+    future = server.send(ping_message, ('localhost', 3001))
+
+    await asyncio.sleep(0.1)
+    assert not future.done()  # we have not yet sent the message
+
+    remote_node = core.Node(addr='localhost', port=3001, nodeid=110)
+
+    pong_message = protocol.create_pong(remote_node, ping_message.nonce)
+    pong_message.nonce = b'garbage'
+
+    mockserver.send(('localhost', 3000), pong_message)
+
+    await asyncio.sleep(0.1)
+    assert not future.done()  # we sent a message with the wrong nonce
+
+    pong_message = protocol.create_pong(remote_node, ping_message.nonce)
+
+    mockserver.send(('localhost', 3000), pong_message)
+
+    # we sent a conforming PONG, the future should now be set!
+    try:
+        await asyncio.wait_for(future, timeout=0.1)
+    except asyncio.TimeoutError:
+        assert False, 'the future was never set'
+
+    assert future.done()
+
+
+@pytest.mark.asyncio
+async def test_incoming_messages_notify_routing_table():
+    'When we receive a message we tell the routing table about it'
+    mockserver = await startmockserver()
+
+    # 1. Start an empty server
+    server = protocol.Server(k=2, mynodeid=0b1000)
+    await server.listen(3000)
+
+    remoteid = 0b1001
+
+    # 2. Send it a PONG
+    remote_node = core.Node(addr='localhost', port=3001, nodeid=remoteid)
+    pong_message = protocol.create_pong(remote_node, b'garbage')
+    mockserver.send(('localhost', 3000), pong_message)
+
+    # 3. Look for the node in the routing table and see that it is good
+    with pytest.raises(KeyError):
+        server.table.last_seen_for(remoteid)
+
+    await asyncio.sleep(0.1)
+    assert server.table.last_seen_for(remoteid) is not None
+
+
 @pytest.mark.asyncio
 async def test_server_send():
     '''
     Test that the server can send something which we can receive
     '''
-    loop = asyncio.get_running_loop()
+    mockserver = await startmockserver()
 
     server = protocol.Server(k=2, mynodeid=0b1000)
     await server.listen(3000)
 
     local_node = core.Node(addr='localhost', port=3000, nodeid=0b1000)
-    remote_addr = ('localhost', 3002)
-    remote_node = core.Node(addr='localhost', port=3002, nodeid=0b1001)
+    remote_addr = ('localhost', 3001)
+    remote_node = core.Node(addr='localhost', port=3001, nodeid=0b1001)
 
     # spin up a server
-    transport, dgprotocol = await loop.create_datagram_endpoint(
-        RecordingDatagramProtocol, local_addr = remote_addr
-    )
-
-    assert len(dgprotocol.messages) == 0
+    assert len(mockserver.messages) == 0
 
     message = protocol.create_ping(local_node)
     server.send(message, remote_addr)
 
     await asyncio.sleep(0.1)
-    assert len(dgprotocol.messages) == 1
+    assert len(mockserver.messages) == 1
 
 
 @pytest.mark.asyncio
@@ -142,35 +148,26 @@ async def test_server_ping():
     When you call server.ping a PING packet is sent.
     It blocks until the PONG packet is received.
     '''
-    loop = asyncio.get_running_loop()
+    mockserver = await startmockserver()
 
     server = protocol.Server(k=2, mynodeid=0b1000)
     await server.listen(3000)
 
-    remote_addr = ('localhost', 3002)
+    remote_addr = ('localhost', 3001)
 
-    # spin up a server
-    transport, dgprotocol = await loop.create_datagram_endpoint(
-        RecordingDatagramProtocol, local_addr = remote_addr
-    )
-
-    assert len(dgprotocol.messages) == 0
-
-    # I don't understand why, but for this test to pass we must create the task after
-    # we create the datagram endpoint
+    assert len(mockserver.messages) == 0
 
     coro = server.ping(remote_addr)
-    task = loop.create_task(coro)
+    task = asyncio.get_running_loop().create_task(coro)
     done, pending = await asyncio.wait({task}, timeout=0.2)
     assert task in pending
 
-    assert len(dgprotocol.messages) == 1
-    ping_message = dgprotocol.messages[0]
+    assert len(mockserver.messages) == 1
+    ping_message = mockserver.messages[0]
 
-    remote_node = core.Node(addr='localhost', port=3002, nodeid=0b1001)
+    remote_node = core.Node(addr='localhost', port=3001, nodeid=0b1001)
     pong_message = protocol.create_pong(remote_node, ping_message.nonce)
-    serialized = pong_message.SerializeToString()
-    transport.sendto(serialized, ('localhost', 3000))
+    mockserver.send(('localhost', 3000), pong_message)
 
     # now that we've sent a PONG it should be unblocked
     done, pending = await asyncio.wait({task}, timeout=0.2)
@@ -179,29 +176,23 @@ async def test_server_ping():
 @pytest.mark.asyncio
 async def test_response_to_ping():
     'When you run a Server and send it a PING it responds with a PONG'
-    loop = asyncio.get_running_loop()
+    mockserver = await startmockserver()
 
     server = protocol.Server(k=2, mynodeid=0b1000)
     await server.listen(3000)
 
-    remote_addr = ('localhost', 3002)
-    transport, dgprotocol = await loop.create_datagram_endpoint(
-        RecordingDatagramProtocol, local_addr = remote_addr
-    )
-
-    remote_node = core.Node(addr='localhost', port=3002, nodeid=0b1001)
+    remote_node = core.Node(addr='localhost', port=3001, nodeid=0b1001)
     ping = protocol.create_ping(remote_node)
-    serialized = ping.SerializeToString()
-    transport.sendto(serialized, ('localhost', 3000))
+    mockserver.send(('localhost', 3000), ping)
 
     # we've sent a ping to the local node, once we give it a chance to run it should send
     # a pong back to us
 
-    assert len(dgprotocol.messages) == 0
+    assert len(mockserver.messages) == 0
     await asyncio.sleep(0.1)
-    assert len(dgprotocol.messages) == 1
+    assert len(mockserver.messages) == 1
 
-    pong = dgprotocol.messages[0]
+    pong = mockserver.messages[0]
     assert pong.nonce == ping.nonce
     assert pong.sender.ip == 'localhost'
     assert pong.sender.port == 3000
@@ -210,31 +201,25 @@ async def test_response_to_ping():
 @pytest.mark.asyncio
 async def test_responds_to_find_node():
     'When you run a Server and send it FIND_NODE it gives you all it has'
-    loop = asyncio.get_running_loop()
+    mockserver = await startmockserver()
 
     server = protocol.Server(k=2, mynodeid=0b1000)
     await server.listen(3000)
 
-    remote_addr = ('localhost', 3002)
-    transport, dgprotocol = await loop.create_datagram_endpoint(
-        RecordingDatagramProtocol, local_addr = remote_addr
-    )
-
     # Ask it for some random node
-    remote_node = core.Node(addr='localhost', port=3002, nodeid=0b1001)
+    remote_node = core.Node(addr='localhost', port=3001, nodeid=0b1001)
 
     request = protocol.create_find_node(remote_node, targetnodeid=0b10000)
-    serialized = request.SerializeToString()
-    transport.sendto(serialized, ('localhost', 3000))
+    mockserver.send(('localhost', 3000), request)
 
     # We should get a response back!
-    assert len(dgprotocol.messages) == 0
+    assert len(mockserver.messages) == 0
     await asyncio.sleep(0.1)
-    assert len(dgprotocol.messages) == 1
+    assert len(mockserver.messages) == 1
 
     # It should return a single element, us! (by sending it a message we added ourselves
     # to the routing table)
-    response = dgprotocol.messages[0]
+    response = mockserver.messages[0]
     assert response.nonce == request.nonce
     assert len(response.findNodeResponse.neighbors) == 1
 
@@ -244,30 +229,24 @@ async def test_responds_to_find_node():
 @pytest.mark.asyncio
 async def test_responds_to_store():
     'When you run a Server and send it STORE it responds and also stores'
-    loop = asyncio.get_running_loop()
+    mockserver = await startmockserver()
 
     server = protocol.Server(k=2, mynodeid=0b1000)
     await server.listen(3000)
 
-    remote_addr = ('localhost', 3002)
-    transport, dgprotocol = await loop.create_datagram_endpoint(
-        RecordingDatagramProtocol, local_addr = remote_addr
-    )
-
     # Ask it for some random node
-    remote_node = core.Node(addr='localhost', port=3002, nodeid=0b1001)
+    remote_node = core.Node(addr='localhost', port=3001, nodeid=0b1001)
 
     request = protocol.create_store(remote_node, key=b'abc', value=b'abc')
-    serialized = request.SerializeToString()
-    transport.sendto(serialized, ('localhost', 3000))
+    mockserver.send(('localhost', 3000), request)
 
     # We should get a response back!
-    assert len(dgprotocol.messages) == 0
+    assert len(mockserver.messages) == 0
     await asyncio.sleep(0.1)
-    assert len(dgprotocol.messages) == 1
+    assert len(mockserver.messages) == 1
 
     # It should return a StoreResponse
-    response = dgprotocol.messages[0]
+    response = mockserver.messages[0]
     assert response.nonce == request.nonce
     assert response.HasField('storeResponse')
 
@@ -279,63 +258,50 @@ async def test_responds_to_store():
 @pytest.mark.asyncio
 async def test_responds_to_find_value_when_no_value():
     'When you run a Server and send it just FIND_VALUE it gives you nearby nodes'
-    loop = asyncio.get_running_loop()
+    mockserver = await startmockserver()
 
     server = protocol.Server(k=2, mynodeid=0b1000)
     await server.listen(3000)
 
-    remote_addr = ('localhost', 3002)
-    transport, dgprotocol = await loop.create_datagram_endpoint(
-        RecordingDatagramProtocol, local_addr = remote_addr
-    )
-
-    remote_node = core.Node(addr='localhost', port=3002, nodeid=0b1001)
+    remote_node = core.Node(addr='localhost', port=3001, nodeid=0b1001)
     request = protocol.create_find_value(remote_node, key=b'abc')
-    serialized = request.SerializeToString()
-    transport.sendto(serialized, ('localhost', 3000))
+    mockserver.send(('localhost', 3000), request)
 
     # We should get a response back!
-    assert len(dgprotocol.messages) == 0
+    assert len(mockserver.messages) == 0
     await asyncio.sleep(0.1)
-    assert len(dgprotocol.messages) == 1
+    assert len(mockserver.messages) == 1
 
     # It should return a FindNodeResponse
-    response = dgprotocol.messages[0]
+    response = mockserver.messages[0]
     assert response.nonce == request.nonce
     assert response.HasField('findNodeResponse')
 
 @pytest.mark.asyncio
 async def test_responds_to_find_value_when_has_value():
     'When you run a Server and send it STORE / FIND_VALUE it gives you the value'
-    loop = asyncio.get_running_loop()
+    mockserver = await startmockserver()
 
     server = protocol.Server(k=2, mynodeid=0b1000)
     await server.listen(3000)
 
-    remote_addr = ('localhost', 3002)
-    transport, dgprotocol = await loop.create_datagram_endpoint(
-        RecordingDatagramProtocol, local_addr = remote_addr
-    )
-
-    remote_node = core.Node(addr='localhost', port=3002, nodeid=0b1001)
+    remote_node = core.Node(addr='localhost', port=3001, nodeid=0b1001)
 
     request = protocol.create_store(remote_node, key=b'abc', value=b'abc')
-    serialized = request.SerializeToString()
-    transport.sendto(serialized, ('localhost', 3000))
+    mockserver.send(('localhost', 3000), request)
 
-    assert len(dgprotocol.messages) == 0
+    assert len(mockserver.messages) == 0
     await asyncio.sleep(0.1)
-    assert len(dgprotocol.messages) == 1
+    assert len(mockserver.messages) == 1
 
     request = protocol.create_find_value(remote_node, key=b'abc')
-    serialized = request.SerializeToString()
-    transport.sendto(serialized, ('localhost', 3000))
+    mockserver.send(('localhost', 3000), request)
 
     await asyncio.sleep(0.1)
-    assert len(dgprotocol.messages) == 2
+    assert len(mockserver.messages) == 2
 
     # It should return a FoundValue
-    response = dgprotocol.messages[1]
+    response = mockserver.messages[1]
     assert response.nonce == request.nonce
     assert response.HasField('foundValue')
     assert response.foundValue.key == b'abc'
